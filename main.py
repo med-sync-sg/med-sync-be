0d1a2bbe55fe7d4326cfbe85be4fb26ea7cd1c57
robinrojh <rygrobin@gmail.com>
2025-01-02 14:27:44 +0800

    Finish up websocket connection with FE client

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ from app.app import app  # Import the FastAPI app instance
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="127.0.0.1", port=8001)
