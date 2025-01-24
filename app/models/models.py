61cf4bd7c3885cdee465f97c7bdd55e227f1f4e5
robinrojh <rygrobin@gmail.com>
2025-01-24 11:45:26 +0800

    Add basic data structure

--- app/models/models.py
+++ app/models/models.py
@@ -2,37 +2,38 @@ import json
 from pydantic import BaseModel, Field
 from typing import Union, Optional, List, Any
 
-class Concept(BaseModel):
-    id : str
-    cui : str
-    name : str
-    definition : str
+def generate_primary_concern_template():
+    root = Entry(key="Primary Concern", value="Main concern of the patient for this visit", root=None, parent_entry=None, child_entries=[])
+    symptoms = Entry(key="Symptoms", value="N/A", parent_entry=root, root=root, child_entries=[]) # The direct child Entry should have the symptom name as the key and other details as its values or child_entries
+    recent_medications = Entry(key="Recent Medications", value="N/A", parent_entry=root, root=root, child_entries=[])
+    vitals = Entry(key="Vitals", value="N/A", parent_entry=root, root=root, child_entries=[])
+    others = Entry(key="others", value="", parent_entry=root, root=root, child_entries=[])
+    root.add_child(symptoms)
+    root.add_child(recent_medications)
+    root.add_child(vitals)
+    root.add_child(others)
+    return root
 
 
-class MedicalEntry(BaseModel):
-    topic: str = Field(default="")
-    entry: Optional[Union[str, int, float, List, 'MedicalEntry', List['MedicalEntry']]] = None
-    parent: Optional['MedicalEntry'] = None
-
-    class Config:
-        json_encoders = {
-            'MedicalEntry': lambda v: v.to_dict(exclude={'parent'})
-        }
-
-class ConsultationModel(BaseModel):
-    entries: List[MedicalEntry]
-    def __init__(self):
-        self.entries = []
-
-class SOAPModel(ConsultationModel):
-    subjective_entries: List[MedicalEntry]
-    objective_entries: List[MedicalEntry]
-    assessment_entries: List[MedicalEntry]
-    plan_entries: List[MedicalEntry]
+class Entry(BaseModel):
+    key: str = Field(default="")
+    value: str | List[str] = Field(default="")
+    root: 'Entry' | None = Field(default=None)
+    parent_entry: 'Entry' | None = Field(default=None)
+    child_entries: List['Entry'] | 'Entry' | None = Field(default=None)
     
-    def __init__(self):
-        super(self)
-        self.subjective_entries = []
-        self.objective_entries = []
-        self.assessment_entries = []
-        self.plan_entries = []
+    def add_child(self, child: 'Entry'):
+        if child == None:
+            raise ValueError("The child cannot be None.")
+        self.child_entries.append(child)
+
+class Section(BaseModel):
+    section_name: str = Field(default="")
+    data: Entry = Field(default=None)
+    
+class PrimaryConcernSection(Section):
+    section_name: str = Field(default="Primary Concern")
+    data: Entry = Field(default_factory=generate_primary_concern_template)
+    
+    
+    
