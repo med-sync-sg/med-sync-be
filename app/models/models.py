553af16d0bf7e5c24dcef81f9582ba48d8f86904
robinrojh <rygrobin@gmail.com>
2025-02-21 12:29:51 +0800

    Add db structure for Template objects

--- app/models/models.py
+++ app/models/models.py
@@ -1,7 +1,11 @@
 from sqlalchemy import Column, String
-from sqlalchemy.dialects.postgresql import JSONB
+from typing import List
+from sqlalchemy.dialects.postgresql import JSONB, ARRAY
 from app.schemas.notes import BaseNote
 from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
@@ -13,20 +17,39 @@ class User(Base):
     email = Column(String, unique=True, index=True, nullable=False)
     age = Column(Integer, nullable=True)
 
-
-class Section(Base):
-    __tablename__ = "sections"
-    id = Column(Integer, primary_key=True)
-    note_id = Column(Integer, ForeignKey("notes.id"), nullable=False)
-    title = Column(String)
-    content = Column(String)
-
-
 class Note(Base):
     __tablename__ = "notes"
 
-    id = Column(Integer, unique=True, primary_key=True)
-    patient_id = Column(String)
+    id: Mapped[int] = mapped_column(primary_key=True)
+    patient_id: Mapped[str] = mapped_column(nullable=False)
     # The entire Pydantic-defined note structure stored as JSON
-    title = Column(String, nullable=False)
-    sections = Column(Integer, ForeignKey("sections.id"), nullable=True)
+    title: Mapped[str] = mapped_column(nullable=False)
+    sections: Mapped[List["Section"]] = relationship(back_populates="note")
+
+class Section(Base):
+    __tablename__ = "sections"
+    
+    id: Mapped[int] = mapped_column(primary_key=True)
+    note_id : Mapped[int] = mapped_column(ForeignKey("notes.id"), nullable=False)
+    note : Mapped["Note"] = relationship(back_populates="sections")
+    title = Column(String)
+    content = Column(JSONB)
+
+class NoteTemplate(Base):
+    __tablename__ = "note_templates"
+    
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(nullable=False)
+    section_templates : Mapped[List["SectionTemplate"]] = relationship(back_populates="note_template")
+    description: Mapped[str] = mapped_column(nullable=False)
+
+class SectionTemplate(Base):
+    __tablename__ = "section_templates"
+    
+    id: Mapped[int] = mapped_column(primary_key=True)
+    description: Mapped[str] = mapped_column(nullable=False)
+    note_template_id : Mapped[int] = mapped_column(ForeignKey("note_templates.id"), nullable=False)
+    note_template: Mapped["NoteTemplate"] = relationship(back_populates="section_templates")
+    title: Mapped[str] = mapped_column(nullable=False)
+    metadata_keys: Mapped[List[str]] = mapped_column(default=[])
+    content_keys: Mapped[List[str]] = mapped_column(default=[])
